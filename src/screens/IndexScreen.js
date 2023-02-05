5c12ad3e5be37ca0e75012d182332145dd1c66a3
ima17 <ima.sudaraka@outlook.com>
2023-02-05 16:26:45 +0530

    Implement add blog posts fucntion

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import BlogContext from "../contexts/BlogContext";
 
 const IndexScreen = () => {
-  return <Text>Index Screen</Text>;
+  const { data, addBlogPosts } = useContext(BlogContext);
+
+  return (
+    <View style={style.container}>
+      <Text>Index Screen</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPosts) => blogPosts.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+      <Button title="Add" onPress={addBlogPosts}/>
+    </View>
+  );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+});
 
 export default IndexScreen;
