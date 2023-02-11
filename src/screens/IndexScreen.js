6e971465f100f7e0b19af9dc16271dacb28d2f04
ima17 <ima.sudaraka@outlook.com>
2023-02-11 22:46:36 +0530

    Add UI changes

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,28 +1,46 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context as BlogContext } from "../contexts/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(BlogContext);
 
   return (
-    <View style={style.container}>
-      <Text>Index Screen</Text>
+    <>
+      <Button title="Add" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPosts) => blogPosts.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={style.row}>
+              <Text style={style.title}>{item.title}</Text>
+              <Feather name="trash" size={24} color="black" />
+            </View>
+          );
         }}
       />
-      <Button title="Add" onPress={addBlogPost} />
-    </View>
+    </>
   );
 };
 
 const style = StyleSheet.create({
-  container: {
-    margin: 20,
+  container: {},
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+    color: "black",
   },
 });
 
