d340ea6f6a9723e941c799316789937ad4052024
ima17 <ima.sudaraka@outlook.com>
2023-02-14 16:16:05 +0530

    Create blog post form

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Text, TextInput, View, StyleSheet, Button } from "react-native";
+
+const BlogPostForm = ({
+  onSubmit,
+  initialValues = {
+    title: "",
+    content: "",
+  },
+}) => {
+  const [title, setTitle] = useState(initialValues.title);
+  const [content, setContent] = useState(initialValues.content);
+
+  return (
+    <View style={styles.conatainer}>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button
+        title="Save blog post"
+        onPress={() => {
+          onSubmit(title, content);
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  conatainer: {
+    margin: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 5,
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: 20,
+  },
+});
+
+export default BlogPostForm;
