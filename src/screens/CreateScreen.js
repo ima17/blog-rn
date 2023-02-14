25d6b7d0e0caa92d4503441f1b5f98979abae8ae
ima17 <ima.sudaraka@outlook.com>
2023-02-14 16:16:37 +0530

    Refactor the code

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,54 +1,24 @@
-import React, { useContext, useState } from "react";
-import { View, Text, TextInput, StyleSheet, Button,TouchableOpacity } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { Context as BlogContext } from "../contexts/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-
   const { addBlogPost } = useContext(BlogContext);
 
   return (
-    <View style={styles.conatainer}>
-      <Text style={styles.label}>Enter Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Enter Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-        title="Add blog post"
-        onPress={() =>
+    <View>
+      <BlogPostForm
+        onSubmit={(title, content) => {
           addBlogPost(title, content, () => {
             navigation.navigate("Index");
-          })
-        }
+          });
+        }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  conatainer: {
-    margin: 10,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "black",
-    padding: 5,
-    fontSize: 18,
-    marginBottom: 15,
-  },
-  label: {
-    marginBottom: 5,
-    fontSize: 20,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
