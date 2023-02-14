f47f042563956199fe09c4bf74fbc2d1ff3b0b76
ima17 <ima.sudaraka@outlook.com>
2023-02-14 21:59:29 +0530

    Create edit blog post function

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,17 +4,19 @@ import { Context as BlogContext } from "../contexts/BlogContext";
 import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation, route }) => {
-  const { state } = useContext(BlogContext);
+  const { state, editBlogPost } = useContext(BlogContext);
 
-  const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
+  const id = route.params.id;
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
-  console.log(blogPost);
   return (
     <View>
       <BlogPostForm
         initialValues={{ title: blogPost.title, content: blogPost.content }}
-        onSubmit={() => {
-          console.log("Test");
+        onSubmit={(title, content) => {
+          editBlogPost(id, title, content, () => {
+            navigation.pop();
+          });
         }}
       />
     </View>
