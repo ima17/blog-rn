4b4429f3e4c8f876ce8977cfc49bd52610a972f2
ima17 <ima.sudaraka@outlook.com>
2023-02-14 14:35:54 +0530

    Add dummy data

--- src/contexts/BlogContext.js
+++ src/contexts/BlogContext.js
@@ -34,5 +34,5 @@ const deleteBlogPost = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   blogReducer,
   { addBlogPost, deleteBlogPost },
-  []
+  [{ title: "Test", content: "This is for test the application", id: "1" }]
 );
