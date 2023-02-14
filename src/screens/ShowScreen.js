a23ca0f78c6bb75997a366b2f65834069405da07
ima17 <ima.sudaraka@outlook.com>
2023-02-14 14:37:31 +0530

    Add edit screen navigation

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,14 +1,29 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { Context as BlogContext } from "../contexts/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
-const ShowScreen = ({ route }) => {
+const ShowScreen = ({ route, navigation }) => {
   const { state } = useContext(BlogContext);
 
   const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Edit", { id: route.params.id })}
+        >
+          <EvilIcons name="pencil" size={35} color="black" />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <View>
       <Text>{blogPost.title}</Text>
+      <Text>{blogPost.content}</Text>
     </View>
   );
 };
